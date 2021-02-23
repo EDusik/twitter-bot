ca848a73da4fa3d4042c3117004f73672939a7cd
EDusik <eduardodusik@gmail.com>
2021-02-22 21:49:21 -0300

    feat(refactor): adding schedule.every().day

--- post_twitter.py
+++ post_twitter.py
@@ -1,5 +1,7 @@
 import tweepy, time, sys
 import settings
+import schedule
+
 from pull_request import get_pull_request
 
 CONSUMER_KEY = settings.ENV['CONSUMER_KEY']
@@ -11,6 +13,11 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-# time.sleep(900) #Tweet every 15 minutes
+def update_twitter():
+  api.update_status(get_pull_request())
 
-api.update_status(get_pull_request())
+schedule.every().day.at("23:59").do(update_twitter)
+
+while 1:
+  schedule.run_pending()
+  time.sleep(1)
